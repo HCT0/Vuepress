08a4bfbf7bf4c8c5ba7f4a78fa8cfbc045eadc4e
HCT0 <2198025618@qq.com>
2021-11-18 11:04:23 +0800

    resove 404

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/hct/', // 这是部署到github相关的配置
+    base: '/hct', // 这是部署到github相关的配置
     markdown: {
       lineNumbers: false // 代码块显示行号
     },
@@ -12,7 +12,7 @@ module.exports = {
       nav:[ // 导航栏配置
         {text: 'PHP', link: '/php/' },
         {text: 'VUE', link: '/vue/'},
-        {text: 'Note', link: 'note'}      
+        {text: 'Note', link: '/note/'}      
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
