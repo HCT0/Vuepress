3bdd80ebc72e8c52f5a477f0a3e85d07c2f902aa
HCT0 <2198025618@qq.com>
2021-11-18 22:36:27 +0800

    更新vue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,22 @@
 module.exports = {
-    title: 'HCT11\'s blog',
-    description: '我的个人网站',
+    title: 'LHH\'s blog',
+    description: '龙火火的博客',
+    keywords:"龙火火",
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['meta',{'name':''}]
     ],
-    base: '/hct', // 这是部署到github相关的配置
+    base: '/', // 这是部署到github相关的配置
     markdown: {
-      lineNumbers: false // 代码块显示行号
+      lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
       nav:[ // 导航栏配置
-        {text: 'PHP', link: '/php/' },
-        {text: 'VUE', link: '/vue/'},
-        {text: 'Note', link: '/note/'}      
+        {text: '首页', link: '/' },
+        {text: '前端', link: '/interface' },
+        {text: '后端', link: '/date'},
+        {text: '关于', link: '/about'} ,
+        {text: 'Github', link: 'https://github.com/HCT0'}  ,    
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
